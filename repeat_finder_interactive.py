ae5fde3d62b1bc090d02b1d4f088c5a022347696
sean7130 <seantong1998@gmail.com>
2024-12-15 21:29:03 -0800

    main logic file w interactive input via 'keyboard' pkg

---
+++ repeat_finder_interactive.py
@@ -0,0 +1,130 @@
+import keyboard
+from math import gcd
+
+DEBUG_PRINT = False
+
+_vd = [str(i) for i in range(10)]
+_vd.append(".")
+VALID_DIGITS = set(_vd)
+
+INPUT = ""
+PERIOD_TRACKER = []
+PREVIOUS_CHAR = ""
+
+def print_results():
+    print(PERIOD_TRACKER)
+
+def check_period(p=PERIOD_TRACKER):
+    max_period = 0
+    full_input = p[0]
+    for i in range(len(p)-2, len(p)//2, -1):
+        test_period_str = p[i]
+        test_period = len(test_period_str)
+        if test_period_str == full_input[2*(-test_period):len(full_input)-test_period]:
+            max_period = test_period
+    return max_period
+
+
+def _perform_backspace():
+    global INPUT
+    INPUT = INPUT[:-1]
+    PREVIOUS_CHAR = INPUT[-1] if len(INPUT) > 0 else ""
+    for i in range(len(PERIOD_TRACKER)):
+        PERIOD_TRACKER[i] = PERIOD_TRACKER[i][:-1]
+    if len(PERIOD_TRACKER) > 1: del(PERIOD_TRACKER[-1])
+
+def seperate_repeats(decimals, period):
+    # keep checking rtl for each period, so that it still follows the repeat pattern
+    # this step is actually not needed but it will help precision on long numbers
+    period_string = PERIOD_TRACKER[-(period+1)]
+    i = 1
+    while (period * (i+1)) <= len(decimals) and period_string == decimals[-(period * (i+1)):-(period* i)]:
+        i += 1
+    i -= 1
+    return decimals[:-(period*(i+1))], decimals[-(period*(i+1)):]
+
+
+def _produce_blunt_overall_fraction(non_repeating, non_repeating_factor, repeating, repeating_factor):
+    blunt_d = (10**repeating_factor - 10**non_repeating_factor)
+    blunt_overall_d = blunt_d * (10**non_repeating_factor)
+    blunt_repeating_n = repeating * (10**non_repeating_factor)
+    blunt_n = blunt_repeating_n + (non_repeating * blunt_d)
+    return blunt_overall_d, blunt_n
+
+
+def simplify_fractions(d, n):
+    mutral_gcd = gcd(d, n)
+    return d//mutral_gcd, n//mutral_gcd
+
+
+def produce_fraction(user_input, max_period):
+    if not max_period or len(PERIOD_TRACKER) == 0:
+        return "<No fraction yet>"
+    decimal_and_fraction = PERIOD_TRACKER[0].split('.')
+    if len(decimal_and_fraction) != 2:
+        return "<No fraction yet>"
+    non_repeating, repeating = seperate_repeats(decimal_and_fraction[1], mp)
+
+    # piece together the repeating and non repeating part 
+    non_repeating_int = int(decimal_and_fraction[0] + non_repeating)
+    repeating_int = int(repeating)
+    repeating_int_multiplication_factor = len(repeating) + len(non_repeating)
+    blunt_denominator = (10**repeating_int_multiplication_factor-10**len(non_repeating))
+    # print(f"confirmed_input={PERIOD_TRACKER[0]}, repeating={repeating_int}, non_repeating={non_repeating_int}")
+    # print(f"repeating's factor={repeating_int_multiplication_factor}, non repeating's factor={len(non_repeating)}")
+    # print(f"blunt-fraction: {repeating_int}/{blunt_denominator} = {repeating_int/blunt_denominator}")
+
+    d, n = _produce_blunt_overall_fraction(non_repeating_int, len(non_repeating), repeating_int, repeating_int_multiplication_factor)
+    d, n = simplify_fractions(d, n)
+    # print(f"results: {n}/{d}={n/d}")
+    return f"{n}/{d}={n/d}"
+
+def _clear():
+    global INPUT
+    INPUT = ""
+    PERIOD_TRACKER.clear()
+
+def register_input(user_input):
+    global INPUT
+    global PREVIOUS_CHAR
+    INPUT += user_input
+    for i in range(len(PERIOD_TRACKER)):
+        PERIOD_TRACKER[i] += PREVIOUS_CHAR
+    # only use previous char because the current one may be the one with rounding errors
+    PREVIOUS_CHAR = user_input
+    PERIOD_TRACKER.append("")
+
+
+if __name__ == "__main__":
+    print(f"\r\033[KWelcome! Start typing a repeating decimal to determine its fraction form", end="", flush=True)
+    while True:
+        e = keyboard.read_event()
+        if e.event_type == "down":
+            if e.name == "esc":
+                print("\nbye!")
+                exit(0)
+            elif e.name == "d":
+                print_results()
+            elif e.name == "enter":
+                print("")
+            elif e.name == "c":
+                _clear()
+                # print(f"period str = {PERIOD_TRACKER[-(mp+1)]}")
+                print(f"\r\033[Kcurrent input: <cleared>", end="", flush=True)
+            elif e.name == "backspace":
+                _perform_backspace()
+
+            elif e.name in VALID_DIGITS:
+                register_input(e.name)
+
+            if INPUT != "":
+                mp = check_period()
+            else:
+                mp = 0
+
+            frac = produce_fraction(INPUT, mp)
+            if DEBUG_PRINT:
+                print(f"\r{PERIOD_TRACKER}, max_period={mp}", end="", flush=True)
+            else: 
+                print(f"\r\033[Kcurrent input: {INPUT}, equivalent fraction form: {frac}", end="", flush=True)
+
